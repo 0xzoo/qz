ec47782199529a28b092ec091b7d9607e8cc3414
0xzoo <crypt0z00@proton.me>
2023-11-30 03:02:55 +0000

    save => nextQ

--- src/routes/Qz.tsx
+++ src/routes/Qz.tsx
@@ -344,7 +344,7 @@ export const Qz = () => {
     // }
     // update az
     setLoading(false) // !fix switch to translate animation
-    // nextQ()
+    nextQ()
   }
 
   const handleMcRadio = useCallback((i: string) => {
